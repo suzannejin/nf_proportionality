c88740b9d188acc1a4332e151c1f90b5d3c888ba
suzannejin <suzanne.jin@crg.eu>
2020-11-24 11:47:49 +0100

    .

--- bin/compute_donor_chunk.R
+++ bin/compute_donor_chunk.R
@@ -1,8 +1,3 @@
-
-# library paths
-.libPaths(c("/nfs/software/R/packages","/nfs/users2/cn/ierb/R/", "/nfs/users2/cn/sjin/R/x86_64-redhat-linux-gnu-library/3.3/"))
-
-
 # ========= #
 # ARGUMENTS #
 # ========= #
@@ -65,7 +60,11 @@ option_list = list(
               type="integer",
               default=100,
               help="Size for propr.chunk",
-              metavar="number")
+              metavar="number"),
+  make_option("--local",
+              action="store_true",
+              default=FALSE,
+              help="Load local libraries.")
 ); 
  
 opt_parser = OptionParser(option_list=option_list)
@@ -74,6 +73,11 @@ opt = parse_args(opt_parser)
 # ========= #
 # LIBRARIES #
 # ========= #
+
+if (opt$local){
+  # library paths
+  .libPaths(c("/nfs/software/R/packages","/nfs/users2/cn/ierb/R/", "/nfs/users2/cn/sjin/R/x86_64-redhat-linux-gnu-library/3.3/"))
+}
 
 library(recount)
 library(zCompositions)
@@ -95,7 +99,7 @@ blocks2combs <- function(counts, n){
 
   # check size
   if (length(unique(ngroup)) <= 2){
-    stop(paste("chunk size ", n, " is too big for data frame of size [", nrow(counts), "][", ncol(counts), "]", sep=""))
+    stop(paste("ERROR: chunk size ", n, " is too big for data frame of size [", nrow(counts), "][", ncol(counts), "]", sep=""))
   }
 
   # split groups
@@ -105,19 +109,102 @@ blocks2combs <- function(counts, n){
   combs <- unique(combs) 
   combs <- combs[combs[,1] != combs[,2],]
 
+  print(paste("----runing propr for ", nrow(combs), " chunks of size ", n))
+
   return(list(combs, split))
 }
 
+chunk2propr <- function(i, chunk, metric="rho", ivar=NA, symmetrize=FALSE,
+                        alpha, p=100, interval=seq(0.4,0.95,0.01) ){
+
+  print(i)
+
+  # compute propr chunk
+  rho.i <- propr(chunk, metric = metric, ivar = ivar, alpha = alpha, p=p)
+  rho.i <- updateCutoffs(rho.i,interval)
+
+  # get cutoff
+  cutoff <- min(rho.i@fdr[,"cutoff"][which(rho.i@fdr[,"FDR"]<.05)])
+
+  # update interval and recompute, if no reasonable cutoff
+  icut = 1
+  while(cutoff==Inf && icut<5){
+    m <- max(interval)
+    b <- (interval[2]-interval[1])/10
+    print(paste("Re-updating cutoff using a different interval (", m, ",", 1-b, ",", b, ")", sep=""))
+    interval <- seq(m, 1-b, b)
+    rho.i <- updateCutoffs(rho.i,interval)
+    cutoff <- min(rho.i@fdr[,"cutoff"][which(rho.i@fdr[,"FDR"]<.05)])
+    icut = icut+1
+  }
+  if(cutoff==Inf){
+    m <- min(rho.i@fdr[,"FDR"])
+    stop(paste("No cutoff for FDR < 0.05, minimum = ", m, sep=""))
+  }
+
+  # get fdr at cutoff
+  fdr <- rho.i@fdr[,"FDR"][which(rho.i@fdr[,"cutoff"]==cutoff)]
+  print(paste("Obtained cutoff[", cutoff, "] and fdr[", fdr, "] for chunk ", i, sep=""))
+
+  return(list(rho.i@matrix, cutoff, fdr))
+}
+
+chunk2full <- function(counts, RES, split, combs){
+  
+  # define variables
+  QUILT <- matrix(0, ncol(counts), ncol(counts))
+  cutoff <- 0.0
+  fdr <- 0.0
+
+  # collect chunks
+  for(i in 1:nrow(combs)){
+
+    # update cutoff and fdr
+    cutoff <- cutoff + RES[[i]][[2]]
+    fdr <- fdr + RES[[i]][[3]]
+
+    # Fill final matrix with each job
+    batch1 <- split[[combs[i,1]]]
+    batch2 <- split[[combs[i,2]]]
+    patch.i <- c(batch1, batch2)
+    QUILT[patch.i, patch.i] <- RES[[i]][[1]]
+  }
+
+  # average cutoff and fdr
+  cutoff <- cutoff / i
+  fdr <- fdr / i
+
+  # rename columns & rows
+  matrix <- QUILT
+  rownames(matrix) <- colnames(counts)
+  colnames(matrix) <- colnames(counts)
+
+  return(list(matrix, cutoff, fdr))
+}
+
+file2res <- function(RES, combs, dir){
+
+  print(paste("----collecting ", nrow(combs), " chunk files previously saved in ", dir, sep=""))
+
+  for(i in 1:nrow(combs)){
+    file2 <- paste0(dir, "/job-", combs[i,1], "+", combs[i,2], ".csv")
+    csv <- read.csv(file2, row.names = 1, header= TRUE)
+    matrix <- data.matrix(csv)
+    RES[[i]][[1]] <- matrix
+  }
+
+  return(RES)
+}
+
 propr.chunk <- function(counts, metric = c("rho", "phi", "phs", "cor", "vlr"),
-                        ivar = "clr", symmetrize = FALSE, alpha, mat=TRUE, p=100,
-                        n=100, ncores = 1, dir = NA, interval=seq(0.4,0.95,0.01)){
+                        ivar = "clr", symmetrize = FALSE, alpha, p=100,
+                        n=100, ncores = 1, interval=seq(0.3,0.95,0.01),
+                        dir = NA){
   
   # divide data into chunks
   l <- blocks2combs(counts, n)
   combs <- l[[1]]
   split <- l[[2]]
-
-  print(paste("----runing propr for ", nrow(combs), " chunks of size ", n))
   
   # parallelize propr computation for i chunks
   doMC::registerDoMC(cores = ncores)
@@ -125,80 +212,37 @@ propr.chunk <- function(counts, metric = c("rho", "phi", "phs", "cor", "vlr"),
   
   RES <- foreach::foreach(i = 1:nrow(combs)) %dopar% {
 
-    print(i)
-
-    # compute propr chunk
+    # get chunk
     batch1 <- split[[combs[i,1]]]
     batch2 <- split[[combs[i,2]]]
     chunk = subset(clr, select = c(batch1, batch2))
-    rho.i <- propr(chunk, metric = metric, ivar = ivar, symmetrize = symmetrize, alpha = alpha, p=p)
-    rho.i <- updateCutoffs(rho.i,interval)
-    cutoff <- min(rho.i@fdr[,"cutoff"][which(rho.i@fdr[,"FDR"]<.05)])
-    icut = 1
-    while(cutoff==Inf && icut<5){
-      m <- max(interval)
-      b <- (interval[2]-interval[1])/10
-      print(paste("Re-updating cutoff using a different interval (", m, ",", 1-b, ",", b, ")", sep=""))
-      interval <- seq(m, 1-b, b)
-      rho.i <- updateCutoffs(rho.i,interval)
-      cutoff <- min(rho.i@fdr[,"cutoff"][which(rho.i@fdr[,"FDR"]<.05)])
-      icut = icut+1
-    }
-    if(cutoff==Inf){
-      m <- min(rho.i@fdr[,"FDR"])
-      stop(paste("No cutoff for FDR < 0.05, minimum = ", m, sep=""))
-    }
-    fdr <- rho.i@fdr[,"FDR"][which(rho.i@fdr[,"cutoff"]==cutoff)]
-    print(paste("Obtained cutoff[", cutoff, "] and fdr[", fdr, "] for chunk ", i, sep=""))
-    
+
+    # compute propr 
+    l_propr <- chunk2propr(i, chunk, metric=metric, ivar=ivar, symmetrize=symmetrize, alpha=alpha, p=p, interval=interval)
+
     if(is.na(dir)){
-
-      # return propr matrix and cutoff (with FDR<0.05)
-      list(rho.i@matrix, cutoff, fdr)
-      
+      # return propr matrix, cutoff, and concrete FDR
+      l_propr
     }else{
-      
-      # write files ir required
-      file1 <- paste0(dir, "/job-", combs[i,1], "+", combs[i,2], ".csv")
-      file2 <- paste0(dir, "/job-", combs[i,1], "+", combs[i,2], ".RData")
-      write.csv(rho.i@matrix, file = file1)
-      save(rho.i, file = file2)
+      # save data if required
+      file2 <- paste0(dir, "/job-", combs[i,1], "+", combs[i,2], ".csv")
+      print(paste("----saving tmp file[", i, "] to ", file2, sep=""))
+      write.csv(l_propr[[1]], file=file2)
+
+      # return cutoff and FDR
+      list(NULL, l_propr[[2]], l_propr[[3]])
     }
   }
-  
-  if(!is.na(dir)) return(NULL)
 
-  QUILT <- matrix(0, ncol(counts), ncol(counts))
-  cutoff <- 0.0
-  fdr <- 0.0
-  for(i in 1:nrow(combs)){
-    
-    # update cutoff and fdr
-    cutoff <- cutoff + RES[[i]][[2]]
-    fdr <- fdr + RES[[i]][[3]]
-
-    # Fill final matrix with each job
-    if (mat){
-      batch1 <- split[[combs[i,1]]]
-      batch2 <- split[[combs[i,2]]]
-      patch.i <- c(batch1, batch2)
-      QUILT[patch.i, patch.i] <- RES[[i]][[1]]
-    }
+  # collect files
+  if(!is.na(dir)){
+    RES <- file2res(RES, combs, dir)
   }
-  # average cutoff and fdr
-  cutoff <- cutoff / i
-  fdr <- fdr / i
 
-  # rename columns & rows
-  if(mat){
-    matrix <- QUILT
-    rownames(matrix) <- colnames(counts)
-    colnames(matrix) <- colnames(counts)
-  }else{
-    matrix = NULL
-  }
-  
-  return(list(matrix, cutoff, fdr))
+  # merge chunks
+  l_propr <- chunk2full(counts, RES, split, combs)
+
+  return(l_propr)
 }
 
 
@@ -302,11 +346,19 @@ rm(rse_gene)
 
 print("----running proportionality analysis")
 
+# output folder
+if (!dir.exists(opt$outdir)) {dir.create(opt$outdir, recursive=TRUE)}
+dir2 = paste(opt$outdir, "tmp", sep="/")
+if (!dir.exists(dir2)) {dir.create(dir2, recursive=TRUE)}
+
 # CLR transformation
 clr = propr:::proprCLR(M)
 
+#ch = propr.chunk(clr, "rho", ivar=NA, alpha=NA, ncores=3, p=20, n=400, interval=seq(.3, .95, .01), dir=dir2)
+
 # run proportionality analysis per chunk 
-ch = propr.chunk(clr, metric="rho", ivar=NA, alpha=NA, ncores=opt$ncores, p=20, interval=seq(opt$interval_min, opt$interval_max, opt$cutoff_interval), n=opt$chunk_size)
+ch = propr.chunk(clr, metric="rho", ivar=NA, alpha=NA, ncores=opt$ncores, p=20, n=opt$chunk_size,
+                 interval=seq(opt$interval_min, opt$interval_max, opt$cutoff_interval), dir=dir2)
 matrix = ch[[1]]
 cutoff = ch[[2]]
 fdr = ch[[3]]
@@ -333,8 +385,6 @@ results = results[mypairs,]
 # OUTPUT #
 # ====== #
 
-if (!dir.exists(opt$outdir)) {dir.create(opt$outdir, recursive=TRUE)}
-
 # proportionality pairs
 out1 = file.path(opt$outdir, paste(basedonor, "_results.csv", sep=""))
 write.table(results, out1, row.names = FALSE, quote=FALSE, sep=",", dec=".")
@@ -346,3 +396,6 @@ writeLines(as.character(cutoff), out2)
 # fdr
 out3 = file.path(opt$outdir, paste(basedonor, "_fdr.txt", sep=""))
 writeLines(as.character(fdr), out3)
+
+# remove tmp files
+unlink(dir2, recursive=TRUE)
